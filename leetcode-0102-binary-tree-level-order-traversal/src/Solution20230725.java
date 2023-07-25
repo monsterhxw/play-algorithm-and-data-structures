d4813564cea54845f4221d65e0c0126adfa03f43
huangxuewei <huangxueweihxw@gmail.com>
2023-07-25 23:47:15 +0800

    Add current learning

---
+++ leetcode-0102-binary-tree-level-order-traversal/src/Solution20230725.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author huangxuewei
+ * @since 2023/7/24
+ */
+public class Solution20230725 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> level = new LinkedList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode cur = queue.poll();
+                level.add(cur.val);
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            res.add(level);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution20230725 solution = new Solution20230725();
+
+        TreeNode root;
+
+        root = case1();
+        System.out.println("result: " + solution.levelOrder(root));
+
+        root = case2();
+        System.out.println("result: " + solution.levelOrder(root));
+
+        root = null;
+        System.out.println("result: " + solution.levelOrder(root));
+    }
+
+    private static TreeNode case1() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(3);
+        root.right.right = new TreeNode(7);
+        return root;
+    }
+
+    private static TreeNode case2() {
+        TreeNode root = new TreeNode(1);
+        return root;
+    }
+}
